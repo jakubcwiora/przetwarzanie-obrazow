f032bf0c294dded32aad2cf8283a1e1784686d41
jakubcwiora <jakubcwiora@wp.pl>
2025-10-18 21:08:06 +0200

    zagubiony plik

---
+++ Lab2/Zadanie3.3.py
@@ -0,0 +1,22 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('Lab2/faceIq.jpg')
+
+def rotateImage():
+  try:
+    angle = int(input("Podaj kąt obrócenia: ")) # Kąt w stopniach
+    center = (image.shape[1] // 2, image.shape[0] // 2) # Środek obrazka
+    scale = 1.0 # skala pozostanie niezmieniona
+
+    M = cv2.getRotationMatrix2D(center=center, angle=angle, scale=scale)
+    rotated_image = cv2.warpAffine(image, M, (image.shape[1], image.shape[0]))
+
+    cv2.imshow("Obrócony obraz", rotated_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+  except: 
+    print("Podano nieprawidłową wartość")
+    rotateImage()
+
+rotateImage()
